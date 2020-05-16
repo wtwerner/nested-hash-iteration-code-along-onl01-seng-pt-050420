7270f8bee5c0215101dee5e1f21bab956c082c8e
Tommy Werner <wtwerner@gmail.com>
2020-05-16 19:38:45 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,11 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
+ contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      if attribute == :facorite_ice_cream_flavors
-      binding.pry
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
     end
   end
 end
